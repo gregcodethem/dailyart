b3db481fa5e936ab01d6c7e557ca434a8705b08c
Greg Shepley <gregshepley@yahoo.co.uk>
2019-01-06 10:57:20 +0000

    Initial bare bones commit

---
+++ dailyart/dailyart/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for dailyart project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dailyart.settings')
+
+application = get_wsgi_application()
